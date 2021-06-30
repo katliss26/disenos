ef52d3c91edcf0bff967bed34fd7e923d07ddcc4
Lissett Carrero <katliss.26@gmail.com>
2021-06-30 16:54:29 -0400

    Agegar el controlador de animaciones

--- lib/src/pages/animaciones_page.dart
+++ lib/src/pages/animaciones_page.dart
@@ -1,3 +1,5 @@
+import 'dart:math' as Math;
+
 import 'package:flutter/material.dart';
 
 
@@ -22,10 +24,48 @@ class CuadradoAnimado extends StatefulWidget {
   _CuadradoAnimadoState createState() => _CuadradoAnimadoState();
 }
 
-class _CuadradoAnimadoState extends State<CuadradoAnimado> {
+class _CuadradoAnimadoState extends State<CuadradoAnimado> with SingleTickerProviderStateMixin{
+
+  late AnimationController controller;
+  late Animation<double> rotacion;
+
+  @override
+  void initState() {
+    controller = AnimationController(vsync: this, duration: Duration( milliseconds: 4000 ));
+    //rotacion = Tween( begin: 0.0, end: 2.0 * Math.pi ).animate(controller);
+    rotacion = Tween( begin: 0.0, end: 2.0 * Math.pi ).animate(
+      CurvedAnimation(parent: controller, curve: Curves.easeOut)
+    );
+    controller.addListener(() { 
+      /*if(controller.status == AnimationStatus.completed) {
+        controller.reverse();
+      } 
+      /*else if(controller.status == AnimationStatus.dismissed) {
+        controller.forward();
+      }*/*/
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
   @override
   Widget build(BuildContext context) {
-    return _Rectangulo();
+    controller.forward();
+    //controller.repeat();
+    return AnimatedBuilder(
+      animation: controller,
+      //child: _Rectangulo(),
+      builder: (BuildContext context, Widget? child) {
+        return Transform.rotate(
+          angle: rotacion.value,
+          child: _Rectangulo()
+        );
+      },
+    );
   }
 }
 
